ecc9c9212e266ea35b356b7845f631023b1f3c90
Derek_yip <derek.yipchinsan@gmail.com>
2023-06-11 13:02:53 +0800

    edit get request

--- routes/product.router.js
+++ routes/product.router.js
@@ -7,7 +7,17 @@ router.get("/", async(req,res)=>{
     try {
         // const products = await sql`SELECT * FROM product;`;
         const products = await postgre.query("select * from product")
-        res.json({data: products.rows})
+        res.json({productlist: products.rows})
+    } catch (error) {
+        res.json({msg: error.msg})
+    }
+})
+
+router.get("/:id", async(req,res)=>{
+    try {
+        // const products = await sql`SELECT * FROM product;`;
+        const products = await postgre.query(`select * from product where id = ${req.params.id}`)
+        res.json(products.rows)
     } catch (error) {
         res.json({msg: error.msg})
     }
