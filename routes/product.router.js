008497840f5dc0a831ec8e2632fff1ef25840aec
Derek_yip <derek.yipchinsan@gmail.com>
2023-06-18 10:10:51 +0800

    .

--- routes/product.router.js
+++ routes/product.router.js
@@ -7,8 +7,10 @@ router.get("/", async(req,res)=>{
     try {
         const products = await db.promise().query("SELECT * FROM Product")
         // const products = await client.sql`SELECT * FROM product;`;
+        res.status = 200
         res.json({productlist: products[0]})
     } catch (error) {
+        res.status = 400
         res.json({msg: error})
     }
 })
@@ -17,8 +19,10 @@ router.get("/:id", async(req,res)=>{
     try {
         // const products = await sql`SELECT * FROM product;`;
         const products = await db.promise().query(`SELECT * from Product where id = ${req.params.id}`)
+        res.status = 200
         res.json(products)
     } catch (error) {
+        res.status = 400
         res.json({msg: error})
     }
 })
